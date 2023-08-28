bf88b6a94e5ec23acfe9dbf00bad1e8d3a014c1d
ErickGonzagaDev <erickgonzaga.dev@gmail.com>
2023-08-28 13:57:21 -0300

    first commit

---
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -0,0 +1,26 @@
+import styles from './Item.module.scss';
+import TagsPrato from 'components/TagsPrato';
+import { useNavigate } from 'react-router-dom';
+import { Prato } from 'types/Prato';
+
+
+export default function Item(props: Prato) {
+  const { title, description, photo, id } = props;
+  const navigate = useNavigate();
+  return (
+    <div
+      onClick={() => navigate(`/prato/${id}`)}
+      className={styles.item}>
+      <div className={styles.item__imagem}>
+        <img src={photo} alt={title} />
+      </div>
+      <div className={styles.item__descricao}>
+        <div className={styles.item__titulo}>
+          <h2> {title} </h2>
+          <p> {description} </p>
+        </div>
+        <TagsPrato {...props} />
+      </div>
+    </div>
+  );
+}
---
+++ src/routes.tsx
@@ -0,0 +1,33 @@
+import Footer from 'components/Footer';
+import Menu from 'components/Menu';
+import PaginaPadrao from 'components/PaginaPadrao';
+import Cardapio from 'pages/Cardapio';
+import Inicio from 'pages/Inicio';
+import NotFound from 'pages/NotFound';
+import Prato from 'pages/Prato';
+import Sobre from 'pages/Sobre';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const Approuter = () => {
+  return (
+    <main className='container'>
+      <Router>
+        <Menu/>
+
+        <Routes>
+          <Route path='/' element={<PaginaPadrao/>}>
+            <Route index element={<Inicio/>} />
+            <Route path='cardapio' element={<Cardapio/>} />
+            <Route path='sobre' element={<Sobre/>}/>
+          </Route>
+          <Route path='prato/:id' element={<Prato/>}/>
+          <Route path='*' element={<NotFound/>}/>
+
+        </Routes>
+        <Footer/>
+      </Router>
+    </main>
+  );
+};
+
+export default Approuter;
